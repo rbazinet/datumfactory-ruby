9fdacc2a21897f14a6343b2415b9afac7fcd3f82
Rob Bazinet <rbazinet@stillriversoftware.com>
2020-03-06 17:12:23 -0500

    Initial commit

---
+++ lib/datumfactory_ruby.rb
@@ -0,0 +1,6 @@
+require "datumfactory_ruby/version"
+
+module DatumfactoryRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/datumfactory_ruby/version.rb
@@ -0,0 +1,3 @@
+module DatumfactoryRuby
+  VERSION = "0.1.0"
+end
